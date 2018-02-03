37ac403d4d6f082a6011726f079e9fa218ef9e43
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-02-03 19:16:26 +0200

    Add age prompt

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,11 @@
  * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
-
-var a = 6;
-var b = "six";
-var myName = "Shmuley";
-var c = null;
+var name = prompt('What is your name?');
+var age = prompt('How old are you, ' + name + '?');
+if (age >= 18) {
+  alert('Hurray!!! You can now proceed to wathing porn! :)');
+} else {
+  var yearsTillAdult = 18 - age;
+  alert('Sorry, you are not old enough for porn! :(. See you back in ' + yearsTillAdult + ' years')
+}
