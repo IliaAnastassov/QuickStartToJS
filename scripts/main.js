e27ecdf8c2576d871334f64b009cd45b441eb729
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-02-11 01:07:29 +0200

    Refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -7,50 +7,54 @@
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
 function AgePrompt() {
-  var name = prompt('What is your name?');
-  var input = prompt('How old are you, ' + name + '?');
+  var name = prompt("What is your name?");
+  var input = prompt("How old are you," + " " + name + "?");
   var age = parseInt(input, 10);
+
   while (isNaN(age) || age < 0 || age > 100) {
-    age = prompt('Sorry, this is not a valid age. How old are you?');
+    age = prompt("Sorry, this is not a valid age. How old are you?");
   }
   if (age >= 18) {
-    alert('Hurray!!! You can now proceed to wathing porn! :)');
+    alert("Hurray!!! You can now proceed to wathing porn! :)");
   } else {
     var yearsTillAdult = 18 - age;
-    alert('Sorry, you are not old enough for porn! :(. See you back in ' + yearsTillAdult + ' years')
+    alert("Sorry, you are not old enough for porn! :(. See you back in" + " " + yearsTillAdult + " " + "years");
   }
 }
 
 function CapitalPrompt() {
-  var answer = prompt('What is the capital of New York?');
-  if (answer === 'Albany') {
-    alert('You are correct!');
+  var answer = prompt("What is the capital of New York?");
+  if (answer === "Albany") {
+    alert("You are correct!");
   } else {
-    alert('Sorry, ' + answer + ' is not the capital of New York.');
+    alert("Sorry, " + answer + " is not the capital of New York.");
   }
 }
 
 function FavoriteColorPrompt() {
-  var favoriteColor = prompt('What is your favorite color?');
-  if (favoriteColor === 'grey') {
-    alert('You are so gay!');
-  } else if (favoriteColor === 'pink') {
-    alert('I wish your balls shrink!');
+  var favoriteColor = prompt("What is your favorite color?");
+
+  if (favoriteColor === "grey") {
+    alert("You are so gay!");
+  } else if (favoriteColor === "pink") {
+    alert("I wish your balls shrink!");
   }
-  else if (favoriteColor === 'red') {
-    alert('You are as gay as uncle Ted!');
+  else if (favoriteColor === "red") {
+    alert("You are as gay as uncle Ted!");
   }
   else {
-    alert('Meeeeh.');
+    alert("Meeeeh.");
   }
 }
 
+// Fizz Buzz game
 function FizzBuzz(min, max) {
   if (min > max) {
     min ^= max;
     max ^= min;
     min ^= max;
   }
+
   var number = Math.round(Math.random() * (max - min)) + min;
   if (number % 3 === 0 && number !== 0) {
     if (number % 5 === 0) {
