dc8ba61aa3c69dcf3f1dad95e98a88551842c426
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-02-13 12:39:38 +0200

    Add switch statement

---
+++ Quick Start to JavaScript Volume 2/02_begin/scripts/main.js
@@ -0,0 +1,17 @@
+
+var sign = prompt("What is your sign").toLowerCase();
+
+switch (sign) {
+    case "aries":
+        alert("You are stubborn!");
+        break;
+    case "leo":
+        alert("You are selfish!");
+        break;
+    case "cancer":
+        alert("Millions of people die because of you each year!!!");
+        break;
+    default:
+        alert("Meeh...");
+        break;
+}
