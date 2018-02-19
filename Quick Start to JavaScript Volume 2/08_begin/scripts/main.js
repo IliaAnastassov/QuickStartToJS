d32707a16bf04909097a8daa0c8f9c60f78e7882
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-02-19 21:44:28 +0200

    update chapter 8

---
+++ Quick Start to JavaScript Volume 2/08_begin/scripts/main.js
@@ -0,0 +1,10 @@
+var friends = ["Shmuley Boteach", "Dildo Schwaggins", "Sister Whore"];
+
+console.log(friends);
+console.log(friends[1]);
+
+friends.push("Gandalf the Gay");
+console.log(friends);
+
+friends.reverse();
+console.log(friends);
