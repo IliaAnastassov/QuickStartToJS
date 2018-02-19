b649ad2df0a60175252a4103d9aac9426011796f
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-02-19 19:09:41 +0200

    Update chapter 7

---
+++ Quick Start to JavaScript Volume 2/07_begin/scripts/main.js
@@ -0,0 +1,11 @@
+function square(number) {
+    return number * number;
+}
+
+function calculateArea(sideOne, sideTwo) {
+    return sideOne * sideTwo;
+}
+
+console.log(square(254215411235));
+console.log(square("asdf"));
+console.log(calculateArea(37, 89));
