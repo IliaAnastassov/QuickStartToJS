6e7ae341345620863a9d30ff8624ccdc5a5682d3
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-04 15:15:10 +0200

    Refactor chapter 10

--- Quick Start to JavaScript_Volume 3/10_end/scripts/main.js
+++ Quick Start to JavaScript_Volume 3/10_end/scripts/main.js
@@ -17,7 +17,7 @@ var outcome;
 window.alert("One day you wake up and the world as you know has ended.  The dead come to life and prey on the living. There is no hope, no cure, only survival.  Welcome to the zombie apocalypse.");
 
 // Store an array of possible beginning scenarios
-var beginningScenarios = ["You wake up in a hospital.  It is eerily quiet.  You tiptoe to the door and peek out.", "You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.", "Desperate times call for desperate measures.  You see a small convenience store up ahead and decide to loot it for goods." ];
+var beginningScenarios = ["You wake up in a hospital.  It is eerily quiet.  You tiptoe to the door and peek out.", "You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.", "Desperate times call for desperate measures.  You see a small convenience store up ahead and decide to loot it for goods."];
 
 // Random number function that generates a whole number between 0 and range
 function randomNumber(range) {
@@ -35,19 +35,18 @@ var character = {
   health: 5,
   strength: 0,
   stealth: 0,
-  
+
   // Ask the player to choose a name for the character
   name: window.prompt("What is your name?"),
-  
+
   // Ask the player to choose a class for the character
   characterClass: window.prompt("These times test the strengths of our character. What were you before the war? (Choose from: soldier, doctor, or politician)").toLowerCase()
-  
 };
 
 // Check to see if player has entered a name...if not, give them another chance
 if (!character.name) {
   character.name = window.prompt("I don't know what to call you if you don't enter a name. If you don't answer I will be forced to name you myself...Last chance. What is your name?");
-  
+
   // If a name still has not been entered, assign a name to the character
   if (!character.name) {
     window.alert("Well I did try to warn you. Gunther Piddles it is then.");
@@ -73,24 +72,24 @@ var choice = window.prompt("The brave adventurer and former " + character.charac
 
 // Check to see if the player's choice was attack
 if (choice === "attack") {
-  
+
   // If so, check to see if the character's strength is 5
   if (character.strength === 5) {
-    
+
     // If the strength is 5, alert the result
     window.alert("Using only your bare hands, you slay that zombie! Success! You loot the store for goods and find an axe, a pack of batteries, and three cans of Beanie Weenies.");
-    
+
     // Set outcome variable to win
     outcome = "win";
-    
+
     // Increase stats if success
     character.strength++;
-    
+
     // If the character's strength is less than 5, alert a different scenario
   } else if (character.strength <= 5) {
     window.alert("You fearlessly charge the zombie with all your strength.  Using your mighty powers you crush thy foe as if it were a sheet of paper you needed to shred.");
     window.alert("Just kidding. You are immediately bitten by the zombie and die. But not all is lost -- if time spent together makes one friends, then you and the zombie become best friends, and live together for eternity. The end.");
-    
+
     // Set outcome variable to lose
     outcome = "lose";
   }
@@ -98,32 +97,32 @@ if (choice === "attack") {
 
 // Check to see if the player's choice was to sneak by the zombie
 if (choice === "sneak" || choice === "sneak by the zombie") {
-  
+
   //If the character's stealth is 5, they sneakby the zombie
   if (character.stealth === 5) {
     window.alert("You sneak by the zombie, you clever bastard.");
-    
+
     // Set outcome variable to win
     outcome = "win";
   }
-  
+
   //Check to see if the character's stealth is less than 5
   if (character.stealth < 5) {
-    
+
     // If stealth is less than 5, the zombie is alerted but the player can still win if their strength is high enough
     window.alert("You try to slip by the zombie, but you accidentally stumble over a can of Beanie Weenies, alerting the zombie of your existence. It slowly turns its head and shuffles towards you.");
-    
+
     // Check to see if strength is 5
     if (character.strength === 5) {
       window.alert("Although you have alerted the zombie to your presence you are able to crush its skull with your bare hands.");
-      
+
       // Set outcome variable to win
       outcome = "win";
-      
-    // If stealth and strength are less than 5, the player loses
+
+      // If stealth and strength are less than 5, the player loses
     } else {
       window.alert("Weaponless and weak the zombie chomps down on an arm as you flail wildly in its general direction.");
-      
+
       // Set outcome variable to lose
       outcome = "lose";
     }
